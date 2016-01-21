34dd7a0d31e3a2becf74fc4725a731dba3d8b993
davidcunha <davidgoncalvescunha@gmail.com>
2016-01-21 01:13:45 +0000

    FindAll containers

--- test/models/container_test.js
+++ test/models/container_test.js
@@ -1,22 +1,31 @@
 'use strict';
 
 var ContainerFactory = require('models/container')
-  , expect = require('chai').expect;
+  , chai = require('chai')
+  , expect = chai.expect
+  , sinon = require('sinon')
+  , chaiAsPromised = require("chai-as-promised");
 
 describe('ContainerFactory', function() {
+  var container;
+
+  before(function() {
+    container = ContainerFactory.getInstance();
+  });
+
   describe('#constructor', function() {
-    var container;
-
-    before(function() {
-      container = ContainerFactory.getInstance();
-    });
-
-    it('instantiate a Container that inherits from SQliteAdapter', function() {
+    it('returns an instantiated container and its schema attributes', function() {
       expect(container.schemaAttrs).to.eql(['id', 'container_name']);
     });
 
-    it('the Container tableName is containers', function() {
+    it('returns an instantiated container and its table name', function() {
       expect(container.tableName).to.eql('containers');
+    });
+  });
+
+  describe('#findAll()', function() {
+    it('returns all containers', function() {
+      return expect(container.findAll()).to.be.fulfilled;
     });
   });
 });
