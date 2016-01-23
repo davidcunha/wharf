bc44fe32b4d3ad8ff18db0494a8d1438fac6dad2
davidcunha <davidgoncalvescunha@gmail.com>
2016-01-23 02:07:40 +0000

    Test container find, fix not valid attributes conditional

--- test/models/container_test.js
+++ test/models/container_test.js
@@ -23,6 +23,24 @@ describe('ContainerFactory', function() {
     });
   });
 
+  describe('#find()', function() {
+    it('returns that attributes are empty', function() {
+      return expect(function() {
+        container.find();
+      }).to.throw('attributes are empty');
+    });
+
+    it('returns that attributes are not valid', function() {
+      return expect(function() {
+        container.find({container: 'container_name'});
+      }).to.throw('attributes are not valid');
+    });
+
+    it('returns a container', function() {
+      return expect(container.find({container_name: 'container_name'})).to.be.fulfilled;
+    });
+  });
+
   describe('#findAll()', function() {
     it('returns all containers', function() {
       return expect(container.findAll()).to.be.fulfilled;
