97b4e08b0ab1252dfb3ede0f3cc134ec92f96f8a
davidcunha <davidgoncalvescunha@gmail.com>
2016-01-25 23:28:03 +0000

    Add delete container, updateContainersList, refactory projection sqliteAdapter

--- test/services/sqlite_adapter_test.js
+++ test/services/sqlite_adapter_test.js
@@ -8,7 +8,7 @@ describe('SQliteAdapter', function() {
   var sqliteAdapter;
 
   before(function() {
-    sqliteAdapter = new SQliteAdapter();
+    sqliteAdapter = new SQliteAdapter('containers');
     sqliteAdapter.schemaAttrs = ['id', 'container_name'];
   });
 
@@ -33,4 +33,30 @@ describe('SQliteAdapter', function() {
       expect(sqliteAdapter.validateAttrs.apply(sqliteAdapter, [{}])).to.be.false;
     });
   });
+
+  describe('#selection', function() {
+    it('returns selection attributes', function() {
+      var attrs = {container_name: 'container_name'};
+      expect(sqliteAdapter.selection(attrs)).to.be.eql('WHERE container_name = container_name');
+    });
+  });
+
+  describe('#projection', function() {
+    it('returns projection attributes', function() {
+      var attrs = ['id', 'container_name'];
+      expect(sqliteAdapter.projection(attrs)).to.be.eql('id,container_name');
+    });
+
+    it('returns wildcard in projection attributes', function() {
+      var attrs = [];
+      expect(sqliteAdapter.projection(attrs)).to.be.eql('*');
+    });
+  });
+
+  describe('#insertion', function() {
+    it('returns insertion attributes', function() {
+      var attrs = {id: '1', container_name: 'container_name'};
+      expect(sqliteAdapter.insertion(attrs)).to.be.eql('(id,container_name) VALUES (\'1\',\'container_name\')');
+    });
+  });  
 });
