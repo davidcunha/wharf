fd09c6dadbac58c727b51ca5c77cb88631072ab9
davidcunha <davidgoncalvescunha@gmail.com>
2016-02-02 21:43:34 +0000

    WorkerMapper, rename DataFetcher to Worker

---
+++ test/services/worker_mapper.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var WorkerMapper = require('services/worker_mapper').getInstance()
+  , chai = require('chai')
+  , sinon = require('sinon')
+  , expect = chai.expect;
+
+describe('WorkerMapper', function() {
+  describe('.find()', function() {
+    var workerStub;
+
+    before(function() {
+      workerStub = sinon.stub({
+        id: 1,
+        entity_name: 'entity',
+        type: 'memory',
+        process: {}
+      });
+    });
+
+    context('list of workers is empty', function() {
+      it('does not find any worker', function() {
+        expect(WorkerMapper.find(workerStub)).to.not.be.ok;
+      });
+    });
+
+    context('list of workers has workers', function() {
+      before(function() {
+        WorkerMapper.create(workerStub);
+      });
+
+      it('finds a worker by its ID', function() {
+        expect(WorkerMapper.find(workerStub)).to.be.ok;
+      });
+
+      it('does not find worker that is not in the workers list', function() {
+        var workerStubNotPresent = sinon.stub({
+          id: 2,
+          entity_name: 'entity',
+          type: 'memory',
+          process: {}
+        });
+
+        expect(WorkerMapper.find(workerStubNotPresent)).to.not.be.ok;
+      });
+    });
+  });
+});
