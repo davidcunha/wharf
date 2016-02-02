6750db8b7faa950cb828a291db12cb9a6f92f59e
davidcunha <davidgoncalvescunha@gmail.com>
2016-02-02 23:21:40 +0000

    Destroy worker from list

--- test/services/worker_mapper.js
+++ test/services/worker_mapper.js
@@ -6,58 +6,62 @@ var WorkerMapper = require('services/worker_mapper').getInstance()
   , expect = chai.expect;
 
 describe('WorkerMapper', function() {
-  describe('.find()', function() {
-    var workerStub;
+  var workerStubs = [];
 
-    before(function() {
-      workerStub = sinon.stub({
-        id: 1,
+  before(function() {
+    for(var i = 1; i <= 3; i++) {
+      workerStubs.push(sinon.stub({
+        id: i,
         entity_name: 'entity',
         type: 'memory',
-        process: {}
-      });
-    });
+        process: {},
+        interval: sinon.useFakeTimers().tick(1000)
+      }));
+    }
+  });
 
+  describe('.find()', function() {
     context('list of workers is empty', function() {
       it('does not find any worker', function() {
-        expect(WorkerMapper.find(workerStub)).to.not.be.ok;
+        expect(WorkerMapper.find(workerStubs[0])).to.not.be.ok;
       });
     });
 
     context('list of workers has workers', function() {
       before(function() {
-        WorkerMapper.create(workerStub);
+        WorkerMapper.create(workerStubs[0]);
       });
 
       it('finds a worker by its ID', function() {
-        expect(WorkerMapper.find(workerStub)).to.be.ok;
+        expect(WorkerMapper.find(workerStubs[0])).to.be.ok;
       });
 
-      it('does not find worker that is not in the workers list', function() {
-        var workerStubNotPresent = sinon.stub({
-          id: 2,
-          entity_name: 'entity',
-          type: 'memory',
-          process: {}
-        });
-
-        expect(WorkerMapper.find(workerStubNotPresent)).to.not.be.ok;
+      it('does not find a worker', function() {
+        expect(WorkerMapper.find(workerStubs[1])).to.not.be.ok;
       });
     });
   });
 
   describe('.findAll()', function() {
     before(function() {
-      WorkerMapper.create(sinon.stub({
-        id: 2,
-        entity_name: 'entity',
-        type: 'memory',
-        process: {}
-      }));
+      WorkerMapper.create(workerStubs[1]);
     });
 
     context('list of workers has workers', function() {
       it('finds workers', function() {
+        expect(WorkerMapper.findAll()).to.have.length(2);
+      });
+    });
+  });
+
+  describe('.destroy()', function() {
+    before(function() {
+      WorkerMapper.create(workerStubs[2]);
+    });
+
+    context('list of workers has workers', function() {
+      it('destroys a worker', function() {
+        expect(WorkerMapper.destroy({id: workerStubs[2].id})).to.be.true;
         expect(WorkerMapper.findAll()).to.have.length(2);
       });
     });
