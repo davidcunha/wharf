2248bc4b1ee9d0ef9de8a4e6c75bc8cd02f8d24e
davidcunha <davidgoncalvescunha@gmail.com>
2016-02-09 23:10:40 +0000

    Worker fetch memory stats from Docker and updates it

--- test/models/stats_worker_test.js
+++ test/models/stats_worker_test.js
@@ -14,16 +14,12 @@ describe('StatsWorker', function() {
       expect(function() { new StatsWorker({id: 1}) }).to.throw(Error, '\'entityName\' is undefined');
     });
 
-    it('returns error if type is missing', function() {
-      expect(function() { new StatsWorker({id: 1, entityName: 'entityName'}) }).to.throw(Error, '\'type\' is undefined');
-    });
-
     it('returns error if process is missing', function() {
-      expect(function() { new StatsWorker({id: 1, entityName: 'entityName', type: 'memory'}) }).to.throw(Error, '\'process\' is undefined');
+      expect(function() { new StatsWorker({id: 1, entityName: 'entityName'}) }).to.throw(Error, '\'process\' is undefined');
     });
 
     it('returns a valid worker', function() {
-      expect(new StatsWorker({id: 1, entityName: 'entityName', type: 'memory', process: {}})).to.be.ok;
+      expect(new StatsWorker({id: 1, entityName: 'entityName', process: {}})).to.be.ok;
     });
   });
 });
