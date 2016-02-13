d6065ba276e0f7dc71a168784da05f1cc2a5fba0
davidcunha <davidgoncalvescunha@gmail.com>
2016-02-13 09:17:25 +0000

    Add cluster manager for managing stats mapper and workers, return empty containers list, parse stats

--- test/models/stats_worker_mapper_test.js
+++ test/models/stats_worker_mapper_test.js
@@ -7,62 +7,42 @@ var StatsWorkerMapper = require('models/stats_worker_mapper')
 
 describe('StatsWorkerMapper', function() {
   var statsWorkerStubs = [];
+  var statsWorkerMapper;
 
   before(function() {
-    for(var i = 1; i <= 3; i++) {
+    statsWorkerMapper = sinon.mock(new StatsWorkerMapper()).object;
+    for(var i = 0; i < 2; i++) {
       statsWorkerStubs.push(sinon.stub({
-        id: i,
-        entity_name: 'entity',
-        type: 'memory',
+        entityName: 'entityName' + i,
         process: {},
         interval: sinon.useFakeTimers().tick(1000)
       }));
+
+      statsWorkerMapper.create(statsWorkerStubs[i].process, statsWorkerStubs[i].entityName);
     }
   });
 
   describe('.find()', function() {
-    context('list of workers is empty', function() {
-      it('does not find any worker', function() {
-        expect(StatsWorkerMapper.find(statsWorkerStubs[0])).to.not.be.ok;
-      });
-    });
-
     context('list of workers has workers', function() {
-      before(function() {
-        StatsWorkerMapper.create(statsWorkerStubs[0]);
-      });
-
       it('finds a worker by its ID', function() {
-        expect(StatsWorkerMapper.find(statsWorkerStubs[0])).to.be.ok;
-      });
-
-      it('does not find a worker', function() {
-        expect(StatsWorkerMapper.find(statsWorkerStubs[1])).to.not.be.ok;
+        expect(statsWorkerMapper.find(statsWorkerStubs[0])).to.be.ok;
       });
     });
   });
 
   describe('.findAll()', function() {
-    before(function() {
-      StatsWorkerMapper.create(statsWorkerStubs[1]);
-    });
-
     context('list of workers has workers', function() {
       it('finds workers', function() {
-        expect(StatsWorkerMapper.findAll()).to.have.length(2);
+        expect(statsWorkerMapper.findAll()).to.have.length(2);
       });
     });
   });
 
-  describe('.destroy()', function() {
-    before(function() {
-      StatsWorkerMapper.create(statsWorkerStubs[2]);
-    });
-
+  describe('.kill()', function() {
     context('list of workers has workers', function() {
-      it('destroys a worker', function() {
-        expect(StatsWorkerMapper.destroy({id: statsWorkerStubs[2].id})).to.be.true;
-        expect(StatsWorkerMapper.findAll()).to.have.length(2);
+      it('kills a worker', function() {
+        expect(statsWorkerMapper.kill({entityName: statsWorkerStubs[1].entityName})).to.be.true;
+        expect(statsWorkerMapper.findAll()).to.have.length(1);
       });
     });
   });
--- test/models/stats_worker_test.js
+++ test/models/stats_worker_test.js
@@ -6,20 +6,16 @@ var StatsWorker = require('models/stats_worker')
 
 describe('StatsWorker', function() {
   describe('.constructor', function() {
-    it('returns error if id is missing', function() {
-      expect(function() { new StatsWorker() }).to.throw(Error, '\'id\' is undefined');
-    });
-
     it('returns error if entityName is missing', function() {
-      expect(function() { new StatsWorker({id: 1}) }).to.throw(Error, '\'entityName\' is undefined');
+      expect(function() { new StatsWorker() }).to.throw(Error, '\'entityName\' is undefined');
     });
 
     it('returns error if process is missing', function() {
-      expect(function() { new StatsWorker({id: 1, entityName: 'entityName'}) }).to.throw(Error, '\'process\' is undefined');
+      expect(function() { new StatsWorker({entityName: 'entityName'}) }).to.throw(Error, '\'process\' is undefined');
     });
 
     it('returns a valid worker', function() {
-      expect(new StatsWorker({id: 1, entityName: 'entityName', process: {}})).to.be.ok;
+      expect(new StatsWorker({entityName: 'entityName', process: {}})).to.be.ok;
     });
   });
 });
